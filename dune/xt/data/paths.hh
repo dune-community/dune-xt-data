758e9e86eb5875f85d2598aebdbb721e5c3bc5db
René Milk <rene.milk@wwu.de>
2018-08-13 10:33:23 +0200

    add minimal test fpr paths

--- dune/xt/data/paths.hh
+++ dune/xt/data/paths.hh
@@ -1,3 +1,5 @@
+#ifndef DUNE_XT_DATA_DUNE_XT_DATA_PATHS_HH
+#define DUNE_XT_DATA_DUNE_XT_DATA_PATHS_HH
 #include <string>
 
 namespace Dune {
@@ -10,3 +12,5 @@ inline std::string spe10_model2_filename();
 } // namespace Data
 } // namespace XT
 } // namespace Dune
+
+#endif // DUNE_XT_DATA_DUNE_XT_DATA_PATHS_HH
---
+++ dune/xt/data/test/paths.cc
@@ -0,0 +1,40 @@
+// This file is part of the dune-xt-common project:
+//   https://github.com/dune-community/dune-xt-common
+// Copyright 2009-2018 dune-xt-common developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2012, 2014, 2016 - 2017)
+//   Rene Milk       (2012 - 2013, 2015 - 2016, 2018)
+//   Tobias Leibner  (2014, 2016)
+
+/**
+ * This file is intended as a starting point for quick testing.
+ */
+
+#include <dune/xt/common/test/main.hxx>
+
+#include <boost/filesystem.hpp>
+
+#include <dune/xt/data/paths.hh>
+
+using namespace boost::filesystem;
+
+void test_file(std::string fn)
+{
+    path filepath{fn};
+
+    EXPECT_TRUE(exists(filepath));
+    EXPECT_TRUE(is_regular_file(filepath));
+    EXPECT_GE(file_size(filepath), 1);
+    std::string content;
+    load_string_file(filepath, content);
+}
+
+GTEST_TEST(paths, main)
+{
+    using namespace Dune::XT::Data;
+    test_file(spe10_model1_filename());
+    test_file(spe10_model2_filename());
+}
