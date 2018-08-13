631a8428c709d6370b79cce5c4316cc28983dde0
René Milk <rene.milk@wwu.de>
2018-08-13 20:28:34 +0200

    apply license

--- dune/xt/data/test/paths.cc
+++ dune/xt/data/test/paths.cc
@@ -1,13 +1,11 @@
-// This file is part of the dune-xt-common project:
-//   https://github.com/dune-community/dune-xt-common
-// Copyright 2009-2018 dune-xt-common developers and contributors. All rights reserved.
-// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
-//          with "runtime exception" (http://www.dune-project.org/license.html)
+// This file is part of the dune-xt-data project:
+//   https://github.com/dune-community/dune-xt-data
+// Copyright 2018 dune-xt-data developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause) 
+// or  GPL-2.0+ (http://opensource.org/licenses/gpl-license) 
+// with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
-//   Felix Schindler (2012, 2014, 2016 - 2017)
-//   Rene Milk       (2012 - 2013, 2015 - 2016, 2018)
-//   Tobias Leibner  (2014, 2016)
+//   Rene Milk (2018)
 
 /**
  * This file is intended as a starting point for quick testing.
