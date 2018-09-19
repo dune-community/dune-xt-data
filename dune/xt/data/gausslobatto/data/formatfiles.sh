8668818754df182fd73cec1538c0bbe233c8851c
Tobias Leibner <tobias.leibner@googlemail.com>
2018-09-19 09:02:20 +0200

    [gausslobatto] remove unused files

--- dune/xt/data/gausslobatto/data/formatfiles.sh
+++
@@ -1,9 +0,0 @@
-for i in $( seq 2 100)
-do
-        fname=gausslobatto${i}.txt
-        sed -e 's/^/{/' -e 's/$/}/' -e '1s/^/{/' -e 's/$/,/' -e '$ s/,$/}/' -e 's/\t/,/' ${fname} > tmp_file.txt
-        cp quad_template.cxx gausslobatto$i.cxx
-        sed -i -e '/PASTEBELOW/r tmp_file.txt' -e '/PASTEBELOW/d' -e "s/ORDER/${i}/" gausslobatto$i.cxx 
-done
-rm tmp_file.txt
-
