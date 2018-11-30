55fc6ba415a25d4a95a3c16a9345b564bbbd8ec2
René Milk <rene.milk@wwu.de>
2018-11-30 12:08:16 +0100

    [pylicense] apply new config

--- cmake/modules/DuneXtDataMacros.cmake
+++ cmake/modules/DuneXtDataMacros.cmake
@@ -1,10 +1,11 @@
 # ~~~
 # This file is part of the dune-xt-data project:
 #   https://github.com/dune-community/dune-xt-data
-# Copyright 2018 dune-xt-data developers and contributors. All rights reserved.
+# Copyright 2009-2018 dune-xt-data developers and contributors. All rights reserved.
 # License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-# or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
-# with "runtime exception" (http://www.dune-project.org/license.html)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
 # Authors:
-#   Rene Milk (2018)
+#   René Fritze (2018)
 # ~~~
+
--- dune/xt/data/lebedev/lebedev_data.hh
+++ dune/xt/data/lebedev/lebedev_data.hh
@@ -1,3 +1,13 @@
+// This file is part of the dune-xt-data project:
+//   https://github.com/dune-community/dune-xt-data
+// Copyright 2009-2018 dune-xt-data developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   René Fritze    (2018)
+//   Tobias Leibner (2018)
+
 #ifndef DUNE_XT_DATA_DUNE_XT_DATA_LEBEDEV_LEBEDEV_DATA_HH
 #define DUNE_XT_DATA_DUNE_XT_DATA_LEBEDEV_LEBEDEV_DATA_HH
 
